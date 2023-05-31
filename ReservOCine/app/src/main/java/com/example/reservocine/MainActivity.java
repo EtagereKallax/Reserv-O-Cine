730704e647dfd1d5e419e97a114af68d0a81c460
Theo Vesque <theovesque171201@gmail.com>
2023-05-31 16:34:37 +0200

    Menu

--- ReservOCine/app/src/main/java/com/example/reservocine/MainActivity.java
+++ ReservOCine/app/src/main/java/com/example/reservocine/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Button boutton;
         boutton = findViewById(R.id.QRcode);
@@ -36,4 +39,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.reservations:
+                startActivity(new Intent(this, QRCode.class));
+                return true;
+            case R.id.accueil:
+                startActivity(new Intent(this, MainActivity.class));
+                return true;
+            case R.id.quitter:
+                finish();
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
 }
